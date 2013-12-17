c12c23e7a64a302f2d4edf37368c4f11bb28c01f
Kapil Sharma <kapil.sharma@silverlineit.com>
2013-12-17 18:06:30 +0530

    Updated library
    send_email function updated
    Mailinblue is replaced with sendinblue
    tutorial.rd updated

--- examples/tutorial.rb
+++ examples/tutorial.rb
@@ -1,4 +1,17 @@
-require 'mailin'
+require './mailin'
 
 m = Mailin.new("https://api.mailinblue.com/v1.0","<your access key>","<your secret key>")
-m.get_campaigns()
+
+# This will initiate the API with the endpoint and your access and secret key.
+
+to = {'to@example.net'=>'to email'} #mandatory
+subject = 'Mon subject' #mandatory
+from = ['from@example.net','from email'] #mandatory
+html = '<h1> hello html </h1>' #mandatory
+text = 'text'
+cc = {'cc@example.net'=>'cc email'}
+bcc = {'bcc@example.net'=>'b cc email'}
+replyto = ['rpely@example.net','reply email']
+attachment=[]
+
+print m.send_email(to,subject,from,html,text,cc,bcc,replyto,attachment)
--- mailin.rb
+++ mailin.rb
@@ -108,8 +108,8 @@ class Mailin
 	def delete_users_list(id,users)
 		return self.delete("list/" + id + "/users",{"users"=> users}.to_json)
 	end
-	def send_email(cc,text,bcc,replyto,html,email_to,attachment,email_from,subject)
-		return self.post("email",{"cc"=> cc,"text"=> text,"bcc"=> bcc,"replyto"=> replyto,"html"=> html,"to"=> email_to,"attachment"=> attachment,"from"=> email_from,"subject"=> subject}.to_json)
+	def send_email(to,subject,from,html,text,cc,bcc,replyto,attachment)
+		return self.post("email",{"cc"=> cc,"text"=> text,"bcc"=> bcc,"replyto"=> replyto,"html"=> html,"to"=> to,"attachment"=> attachment,"from"=> from,"subject"=> subject}.to_json)
 	end
 	def get_webhooks()
 		return self.get("webhook","")
